51cfd5cb5d023e8d777382c7948f60d4624572ad
anildalar <dollorinfotech@gmail.com>
2025-08-22 09:22:00 +0530

    new

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="row">
+          <div className="col-3">
+              <input className="form-control" />
+          </div>
+          <div className="col-3">
+              <input className="form-control" />
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
